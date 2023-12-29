5cdf10d8ce59f8bb56b1d09ad1f4d3876a004e7c
서민재 <hey_minj@naver.com>
2023-12-30 02:13:36 +0900

    아

--- lion/settings/prod.py
+++ lion/settings/prod.py
@@ -1,7 +1,8 @@
 from .base import *
+import os
 
 ALLOWED_HOSTS = ["13.124.78.53"]
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = []
 DEBUG = False
